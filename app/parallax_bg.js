807acb6ccc7445e6468d7ab6afb407dbe3dde34f
mrinmiro <mrinmiro@gmail.com>
2020-03-25 00:52:16 +0200

    Updates in parallax_bg module

--- app/parallax_bg.js
+++ app/parallax_bg.js
@@ -5,15 +5,24 @@ if (typeof m == 'undefined') {
 
 m.fn.parallax_bg = function(context) {
 
-    var delta = parseInt(this.data.delta) || 100;
+    var
+        delta = parseInt(this.data.delta) || 100,
+        computed_style = getComputedStyle(context.first, null),
+        computed_position = computed_style.getPropertyValue('background-position'),
+        computed_position_y = computed_style.getPropertyValue('background-position-y'),
+        pos_arr = computed_position.split(' ');
+
+    if (computed_position.indexOf(' ') === -1 || pos_arr.length < 2) {
+        return false;
+    }
 
     m(window).on('scroll', function(){
 
         var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset :
             (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-        context.first.style.backgroundPosition = 'top ' +
+        context.first.style.backgroundPosition = 'left '+ computed_position_y +' top ' +
             ((scrollTop / (document.body.offsetHeight - window.innerHeight)) * delta)
-            + 'px center';
+            + 'px';
     });
 };
