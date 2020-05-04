f2ef2f3486bec8a72d75dfd65f0758a861246a4e
mrinmiro <mrinmiro@gmail.com>
2020-05-04 18:18:14 +0300

    Added ALT attribute fro SVG image

--- app/circular_svg.js
+++ app/circular_svg.js
@@ -21,5 +21,5 @@ m.fn.circular_svg = function(context) {
         percents *= 100;
     }
 
-    context.html('<svg viewBox="0 0 36 36" class="circular-svg"><path class="bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke="' + color + '" stroke-dasharray="' + percents + ', 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text x="18" y="20.35" class="percentage">' + percents + '%</text></svg>');
+    context.html('<svg viewBox="0 0 36 36" class="circular-svg" alt="'+this.attr('data-m-percents')+'%"><path class="bg" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><path class="circle" stroke="' + color + '" stroke-dasharray="' + percents + ', 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831"/><text x="18" y="20.35" class="percentage">' + percents + '%</text></svg>');
 };
